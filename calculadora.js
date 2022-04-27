3a031d9e717a88960ab2d5fe5ca7b95cd55dee31
fenuchi-sara <sarafenuchi@gmail.com>
2022-04-27 09:30:38 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+function somar(){
+    let n1 = document.getElementById('n1').value;
+    let n2 = document.getElementById('n2').value;
+    document.getElementyByid('resultado').innerHTML = n1 + n2; 
+}
